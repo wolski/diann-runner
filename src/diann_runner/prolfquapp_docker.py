c89c60c7e30c3b3e3f9c49c48efa7a384617d8db
Witold Wolski <wewolski@gmail.com>
2026-02-05 09:07:10 +0100

    2.0.8

--- src/diann_runner/prolfquapp_docker.py
+++ src/diann_runner/prolfquapp_docker.py
@@ -46,7 +46,6 @@ def build_docker_cmd(image_version: str, image_repo: str, argv: list[str]) -> li
         DockerCommandBuilder(image)
         .with_cleanup()
         .with_init()
-        .with_interactive()
         .with_uid_gid(flag="--user")
         .with_mount(os.getcwd(), "/work", style="bind")
         .with_workdir("/work")
